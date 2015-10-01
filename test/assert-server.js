5d626afa0d94b49c99cb000745c1583a74c8a104
Joseph Dykstra <josephdykstra@gmail.com>
2015-10-01 11:13:25 -0500

    Use node's assert module

--- test/assert-server.js
+++ test/assert-server.js
@@ -1,5 +1,6 @@
 var cp = require('child_process')
 var fs = require('fs')
+var assert = require('assert')
 
 var expectStatus = process.argv[2]
 var expectTxtFile = 'test/expect/' + expectStatus + '.txt'
@@ -10,18 +11,18 @@ if (!(/^(pass|fail)$/.test(expectStatus))) {
 }
 
 fs.readFile(expectTxtFile, function (err, expect) {
-	if (err) throw err
+	assert.ifError(err)
 	var child = cp.execFile('node', ['test/server.js', clientFile], function (err, stdout, stderr) {
-		if (err && !err.killed && !err.signal && err.code === 1 && !expectPass) {
-			console.log('expected fail')
-		} else if (err) {
-			throw err
-		} else if (expect.toString() !== stdout.toString()) {
-			throw new Error('neq:\n' + expect + '\n' + stdout)
-		} else if (expectPass) {
-			console.log('expected pass')
+		if (expectPass) {
+			console.log('expect pass')
+			assert.ifError(err)
 		} else {
-			throw new Error('idk what happened but it is probably bad')
+			console.log('expect fail')
+			assert.ok(err)
+			assert.ok(!err.killed)
+			assert.ok(!err.signal)
+			assert.equal(err.code, 1)
 		}
+		assert.equal(expect.toString(), stdout.toString())
 	})
 })
